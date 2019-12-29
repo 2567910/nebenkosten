c9408d4ad20ea2e6b03aff6cfe1fb18c3bf03ca2
Lukas Seyfarth <lukas.seyfarth@blu-beyond.com>
2019-12-29 21:45:05 +0100

    problem: css is not loading on rewrite

--- public/js/plugins/load-places.js
+++ public/js/plugins/load-places.js
@@ -5,8 +5,16 @@ $.fn.loadPlaces = function (firestore) {
 
   firestore.collection("places").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      $("#cards").append("<div class='card-ind shadow-1'><div class='inner-card d-flex flex-wrap'><div class='card-left'><i class='fa fa-home'></i></div><div class='card-right'><h4>Henstraße 3, Weimar 99428</h4><a type='button' class='btn btn-primary text-white' href='place.html'>show details</a></div></div></div>");
-      console.log(`${doc.id} => ${doc.data()}`);
+      $("#cards").append("<div class='card-ind shadow-1'><div class='inner-card d-flex flex-wrap'><div class='card-left'><i class='fa fa-home'></i></div><div class='card-right'><h4>" +
+      `${doc.data().strasse}` + " "+
+      `${doc.data().hausnummer}` +
+      ", " +
+      `${doc.data().stadt}` + " " +
+      `${doc.data().plz}` +
+      "</h4><a class='btn btn-dark text-white' href=" +
+      "'place/" + `${doc.id}`+ "'" +
+      ">Abrechungen anzeigen</a></div></div></div>");
+      console.log(`${doc.id} => ${doc.data().strasse}`);
       console.log(JSON.stringify(doc.data()));
     });
   });
