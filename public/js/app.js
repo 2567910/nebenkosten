46bb9aec8e91da648c88042f025c39a3116fb19f
Lukas Seyfarth <lukas.seyfarth@blu-beyond.com>
2020-01-01 16:55:42 +0100

    hallo jörg

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-//   var config = {
-//     apiKey: "AIzaSyDJHa_qd28muromBAb9RqPyIk12AJevPtQ",
-//     authDomain: "nebenkosten-6b4ec.firebaseapp.com",
-//     databaseURL: "https://databaseName.firebaseio.com",
-//     storageBucket: "bucket.appspot.com"
-//   };
-//   firebase.initializeApp(config);
-
-// Get a reference to the database service
-
-
-
 
 $(document).ready(function () {
     var firestore = firebase.firestore();
@@ -29,7 +15,7 @@ $(document).ready(function () {
     if ($('.needs-validation').length) $().validateForms();
     if ($('#cards').length) $().initCards();
     if ($('#loadplaces').length) $().loadPlaces(firestore);
-    if ($('#newplace').length) $().newPlace();
+    if ($('#newplace').length) $().newPlace(firestore);
     //   var db = firebase.database().ref().child('test');
     //   db.on('value', function(snapshot) {
     //     showPlaces(snapshot.val());
--- public/js/plugins/new-place.js
+++ public/js/plugins/new-place.js
@@ -2,9 +2,24 @@
 
 
 
-  $.fn.newPlace = function () {
-    $('#toBeTranslatedForm').submit(function() {
-        var textareaval = $('#street').val();
-        alert(textareaval);
-     });
+$.fn.newPlace = function (firestore) {
+    $('#toBeTranslatedForm').submit(function () {
+        console.log("form was submittet");
+        firestore.collection("places").add({
+            stadt: $('#city').val(),
+            plz: $('#zip').val(),
+            strasse: $('#street').val(),
+            hausnummer: $('#street-number').val(),
+        })
+            .then(function (docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                document.location.href = "/";
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+
+
+    });
+
 }
